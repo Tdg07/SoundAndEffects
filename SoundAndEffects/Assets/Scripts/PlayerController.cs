d2d8a6c53a53e4708bef8f6614ceed00f2b46de0
Tdg07 <guralchukt@roverkids.org>
2024-12-16 13:37:52 -0500

    3.3 Complete

--- SoundAndEffects/Assets/Scripts/PlayerController.cs
+++ SoundAndEffects/Assets/Scripts/PlayerController.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
+    private Animator playerAnim;
     public float jumpForce = 1;
     public float gravityModifier;
     public bool isOnGround = true;
@@ -12,16 +13,18 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier; 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space) && isOnGround) 
+        if(Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver) 
         {
             playerRb.AddForce(Vector3.up * 10, ForceMode.Impulse);
             isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
         }
     }
 
@@ -31,10 +34,12 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;
-        }  if (collision.gameObject.CompareTag("Obstacle")) 
+        }  else if (collision.gameObject.CompareTag("Obstacle")) 
         {
             Debug.Log("Game Over");
             gameOver = true;
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
         }
     }
 }
